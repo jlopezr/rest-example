0516550a857759fce9fefa00045277d4174855fe
Juan López Rubio <j.lopez.r@gmail.com>
2016-11-15 23:03:55 +0100

    Initial commit

---
+++ src/main/java/edu/upc/dsa/JSONService.java
@@ -0,0 +1,37 @@
+package edu.upc.dsa;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/json")
+public class JSONService {
+
+    @GET
+    @Path("/get")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Track getTrackInJSON() {
+
+        Track track = new Track();
+        track.setTitle("Enter Sandman");
+        track.setSinger("Metallica");
+
+        return track;
+
+    }
+
+    @POST
+    @Path("/post")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createTrackInJSON(Track track) {
+
+        String result = "Track saved : " + track;
+        return Response.status(201).entity(result).build();
+
+    }
+
+}
---
+++ src/main/java/edu/upc/dsa/Main.java
@@ -0,0 +1,49 @@
+package edu.upc.dsa;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.ServerConfiguration;
+import org.glassfish.grizzly.http.server.accesslog.*;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Main class.
+ *
+ */
+public class Main {
+    // Base URI the Grizzly HTTP server will listen on
+    public static final String BASE_URI = "http://localhost:8080/myapp/";
+
+    /**
+     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     * @return Grizzly HTTP server.
+     */
+    public static HttpServer startServer() {
+        // create a resource config that scans for JAX-RS resources and providers
+        // in edu.upc.dsa package
+        final ResourceConfig rc = new ResourceConfig().packages("edu.upc.dsa");
+
+        // create and start a new instance of grizzly http server
+        // exposing the Jersey application at BASE_URI
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+
+    /**
+     * Main method.
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.println(String.format("Jersey app started with WADL available at "
+                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+
+        System.in.read();
+        server.stop();
+    }
+}
+
